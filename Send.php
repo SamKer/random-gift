f588a402984c9d754a0713d3c5362fba93717e18
samir.keriou <samir.keriou@gendarmerie.interieur.gouv.fr>
2022-11-17 16:53:32 +0100

    ajout test & prepare

--- Send.php
+++ Send.php
@@ -2,6 +2,7 @@
 
 namespace RandomGift;
 
+use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -15,89 +16,118 @@ use Symfony\Component\Mailer\Transport\Smtp\SmtpTransport;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Yaml\Yaml;
 
-class Send extends Command {
-
+class Send extends Command
+{
 
 
     public function configure()
     {
-         $this->setName("run")
-		->setDescription('envoi un check')
-		->addOption("test", "t",  InputOption::VALUE_OPTIONAL, "mode test")
-             ;
+        $this->setName("run")
+            ->setDescription('envoi un check')
+            ->addOption("test", "t", InputOption::VALUE_OPTIONAL, "mode test", false)
+            ->addOption("prepare", "p", InputOption::VALUE_OPTIONAL, "envoi un mail de preparation", false)
+        ;
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
         $io->title(PROJECT_NAME);
-        $io->writeln("Send TODO Gift", SymfonyStyle::OUTPUT_NORMAL);
+        $io->writeln("Send TODO Gift");
 
-        $modeTest = $input->getOption("test");
-
+        $mode = "go";
+        if($input->getOption("prepare") !== false) {$mode = "prepare";};
+        if($input->getOption("test") !== false ) {$mode = "test";};
         // processing
         //get conf
         $conf = Yaml::parse(file_get_contents(PROJECT_CONFIG));
 
+
+        $conf['text_test'] = $conf['text_prepare'] . $conf['text_go'];
+
         $ids = array_keys($conf['attendees']);
         $idsTaken = [];
-        $users = array_map(/**
-         * @throws \Exception
-         */ function($k, $v) use ($ids, &$idsTaken, $conf) {
-            $v['id'] = $k;
-            //randomize
-            $toId = null;
-            while ($toId === null) {
-                $rand  = random_int(0, count($ids)-1);
-                $id = $ids[$rand];
-                if($id !== $k && !in_array($id, $idsTaken)) {
-                    $toId = $id;
-                    $idsTaken[] = $toId;
+        $users = array_map(
+        /**
+         * @throws Exception
+         */
+            function ($k, $v) use ($ids, &$idsTaken, $conf) {
+                $v['id'] = $k;
+                //randomize
+                $toId = null;
+                while ($toId === null) {
+                    $rand = random_int(0, count($ids) - 1);
+                    $id = $ids[$rand];
+                    if ($id !== $k && !in_array($id, $idsTaken)) {
+                        $toId = $id;
+                        $idsTaken[] = $toId;
+                    }
                 }
-            }
-            $v['gift_to'] = $conf['attendees'][$toId];
-            return $v;
-        },
-        array_keys($conf['attendees']),
-        $conf['attendees']
-    );
+                $v['gift_to'] = $conf['attendees'][$toId];
+                return $v;
+            },
+            array_keys($conf['attendees']),
+            $conf['attendees']
+        );
 
-
-try {
-
-
-    foreach ($users as $i => $user) {
-        $message = "<div style=''>
-<h3>Random Gift:</h3>
-    <div>
-        <p>Random gift a lancé les dés:</p>
-        <p>Le tirage au sort a décidé que vous devez offrir à %s un cadeau d'une valeur n'excédant pas %s.</p>
-        <p>Les échanges de cadeaux se dérouleront à la rentrée 2022</p>
-        <p>Cordialement, Joyeux Noël</p>
-    </div>
-</div>";
-        $message = sprintf($message, $user['gift_to']['name'], $conf['gift_max']);
-        //send
-        if($modeTest === null) {
+        $io->writeln("calcul done");
+        $io->writeln("sending mail (mode Test= $mode)");
+        try {
+            $msg = $conf["text_$mode"];
             $transport = Transport::fromDsn($conf['mailer_dsn']);
             $mailer = new Mailer($transport);
             $email = new Email();
             $email->from($conf['mailer_admin']);
-            $email->to($user['mail']);
-            $email->subject("Random Gift Loto :: LE VRAI TIRAGE");
-            $email->html($message);
-            $mailer->send($email);
-            $io->writeln("mail sending to " . $user['mail']);
-            sleep(2);
+            $email->to($conf['mailer_admin']);
+            $email->replyTo($conf['mailer_admin']);
+            switch ($mode) {
+                case "prepare":
+                    //mail global
+                    $message = sprintf($msg, $conf['gift_max']);
+                    $email->subject("Random Gift Loto :: LE VRAI TIRAGE (se prépare)");
+                    $email->html($message);
+                    foreach ($users as $i => $user) {
+                        $email->addTo($user['mail']);
+                        $io->writeln("mail sending to " . $user['mail']);
+                    }
+                    $mailer->send($email);
+                    break;
+                case "test":
+                    //mail global
+                    $io->writeln("Test Case");
+                    $message = sprintf($msg, $conf['gift_max'], "test Admin", $conf['gift_max']);
+                    $email->subject("Random Gift Loto :: LE VRAI TIRAGE (test mode)");
+                    $email->html($message);
+                    foreach ($users as $i => $user) {
+                        if (isset($user['test']) && $user['test'] === true) {
+                            $email->addTo($user['mail']);
+                            $io->writeln("mail sending to " . $user['mail']);
+                        }
+                    }
+                    $mailer->send($email);
+                    //sauvegarde local uniquement dans cas go ? et test
+                    file_put_contents(PROJECT_DIR . "/var/gift_" . (new \DateTime())->format("YmdHis") . ".txt", json_encode($users, JSON_PRETTY_PRINT));
+                    break;
+                case "go":
+                default:
+                    // 1 mail /personne
+                    foreach ($users as $user) {
+                        $message = sprintf($msg, $user['gift_to']['name'], $conf['gift_max']);
+                        $email->to($user['mail']);
+                        $email->subject("Random Gift Loto :: LE VRAI TIRAGE");
+                        $email->html($message);
+                        $mailer->send($email);
+                        $io->writeln("mail sending to " . $user['mail']);
+                        sleep(2);
+                    }
+                    //sauvegarde local uniquement dans cas go ? et test
+                    file_put_contents(PROJECT_DIR . "/var/gift_" . (new \DateTime())->format("YmdHis") . ".txt", json_encode($users, JSON_PRETTY_PRINT));
+                    break;
+            }
+
+        } catch (TransportException $e) {
+            $io->error($e->getMessage());
         }
-    }
-
-    //sauvegarde local
-    file_put_contents(PROJECT_DIR . "/var/gift_". (new \DateTime())->format("YmdHis").".txt", json_encode($users, JSON_PRETTY_PRINT));
-
-} catch(TransportException $e) {
-    $io->error($e->getMessage());
-}
         return 0;
     }
 }
